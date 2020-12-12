bcb18831fc4e30848c4d2f14a6d95c09c6b0daaa
Jyotishna Baishya <73800133+JyotishnaBaishya@users.noreply.github.com>
2020-12-12 16:27:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var app = express();
 const expressEjsLayout=require('express-ejs-layouts');
-var session = require('express-session');
-
-app.use(session({
-	secret: '6797090678597979890',
-	resave: true,
-	saveUninitialized: true
-}));
 
 app.set('view engine', 'ejs');
 app.use(expressEjsLayout);
@@ -20,6 +13,9 @@ app.use(bodyParser.json());
 var userRouter = require('./routes/index.js');
 app.use('/', userRouter);
 
-app.listen(3000, () => {
-	console.log('Example app listening at http://localhost:3000');
+app.use(express.static('./add'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
 });
